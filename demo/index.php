9f739760a98dd69bb506eb8acda1060a71a02dcb
Abdulrhman Elbuni <aelbuni@users.noreply.github.com>
2017-05-18 18:57:31 -0400

    Update index.php

--- demo/index.php
+++ demo/index.php
@@ -84,16 +84,6 @@
 	
 </head>
 <body style="background-image:url('images/bg.jpg');background-size:cover">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-4339775-4', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 	<center>
 
